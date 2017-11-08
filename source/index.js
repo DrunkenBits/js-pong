c26516e4134c58c1fc4d9d159c4a578e51642f63
Luca Barone <baro.luc@gmail.com>
2017-11-08 22:39:14 +0100

    canvas init

--- source/index.js
+++ source/index.js
@@ -1,1 +1,17 @@
-console.log('wooot!')
+import {injectGlobal} from 'styled-components'
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+
+  #game {
+    border: 1px solid;
+  }
+`
+
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'green'
+ctx.fillRect(10, 10, 10, 10)
