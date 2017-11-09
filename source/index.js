8cf269607b7746b831960f6103d678ca5891ee5e
Luca Barone <baro.luc@gmail.com>
2017-11-09 03:44:19 +0100

    structure code moar

--- source/index.js
+++ source/index.js
@@ -9,28 +9,55 @@ injectGlobal`
   #game {
     border: 1px solid;
     background-color: black;
+    margin: 0 auto;
+    display: block;
+    margin-top: 2em;
   }
 `
 
-const createDrawBall = ctx => R.curry((x, y) => ctx.fillRect(x, y, 10, 10))
+const createDrawBall = R.curry(({ctx, ballSize}, x, y) =>
+  ctx.fillRect(x, y, ballSize, ballSize),
+)
 
-const createDrawPlayer = R.curry((ctx, canvasWidth, side, y) => {
-  switch (side) {
-    case 'right':
-      ctx.fillRect(canvasWidth - 10, 10, 10, 60)
-      break
-    default:
-      ctx.fillRect(0, 10, 10, 60)
-  }
-})
+const createDrawPlayer1 = R.curry(
+  ({ctx, gameHeight, playerHeight, playerWidth}, y) => {
+    const realY = y + playerHeight > gameHeight ? gameHeight - playerHeight : y
+
+    ctx.fillRect(0, realY, playerWidth, playerHeight)
+  },
+)
+const createDrawPlayer2 = R.curry(
+  ({ctx, gameWidth, gameHeight, playerHeight, playerWidth}, y) => {
+    const realY = y + playerHeight > gameHeight ? gameHeight - playerHeight : y
+
+    ctx.fillRect(gameWidth - playerWidth, realY, playerWidth, playerHeight)
+  },
+)
 
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
-const drawBall = createDrawBall(ctx)
-const drawPlayer1 = createDrawPlayer(ctx, canvas.offsetWidth, 'left')
-const drawPlayer2 = createDrawPlayer(ctx, canvas.offsetWidth, 'right')
+const gameWidth = canvas.offsetWidth
+const gameHeight = canvas.offsetHeight
+const playerHeight = 60
+const playerWidth = 10
+const ballSize = playerWidth
+
+const drawBall = createDrawBall({ctx, ballSize})
+const drawPlayer1 = createDrawPlayer1({
+  ctx,
+  gameHeight,
+  playerWidth,
+  playerHeight,
+})
+const drawPlayer2 = createDrawPlayer2({
+  ctx,
+  gameHeight,
+  gameWidth,
+  playerHeight,
+  playerWidth,
+})
 
 ctx.fillStyle = 'white'
 drawBall(40, 10)
-drawPlayer1(20)
-drawPlayer2(20)
+drawPlayer1(1000)
+drawPlayer2(1000)
